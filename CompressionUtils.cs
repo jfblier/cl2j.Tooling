88dd8915db8e4f190e76f5584558eef470e77c59
Jean-Francois Blier <jfblier@gmail.com>
2023-11-18 06:19:40 -0500

    Added ConflictException Fixed compilation warnings

--- CompressionUtils.cs
+++ CompressionUtils.cs
@@ -9,30 +9,26 @@ namespace cl2j.Tooling
         {
             var bytes = Encoding.UTF8.GetBytes(str);
 
-            using (var msi = new MemoryStream(bytes))
-            using (var mso = new MemoryStream())
+            using var msi = new MemoryStream(bytes);
+            using var mso = new MemoryStream();
+            using (var gs = new GZipStream(mso, CompressionMode.Compress))
             {
-                using (var gs = new GZipStream(mso, CompressionMode.Compress))
-                {
-                    StreamUtils.CopyTo(msi, gs);
-                }
-
-                return mso.ToArray();
+                StreamUtils.CopyTo(msi, gs);
             }
+
+            return mso.ToArray();
         }
 
         public static string Decompress(byte[] bytes)
         {
-            using (var msi = new MemoryStream(bytes))
-            using (var mso = new MemoryStream())
+            using var msi = new MemoryStream(bytes);
+            using var mso = new MemoryStream();
+            using (var gs = new GZipStream(msi, CompressionMode.Decompress))
             {
-                using (var gs = new GZipStream(msi, CompressionMode.Decompress))
-                {
-                    StreamUtils.CopyTo(gs, mso);
-                }
-
-                return Encoding.UTF8.GetString(mso.ToArray());
+                StreamUtils.CopyTo(gs, mso);
             }
+
+            return Encoding.UTF8.GetString(mso.ToArray());
         }
     }
 }
---
+++ Exceptions/ConflictException.cs
@@ -0,0 +1,19 @@
+﻿namespace cl2j.DataStore
+{
+    public class ConflictException : Exception
+    {
+        public ConflictException()
+        {
+        }
+
+        public ConflictException(string message)
+            : base(message)
+        {
+        }
+
+        public ConflictException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
